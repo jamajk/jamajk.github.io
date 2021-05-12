731f4962f96890994caeafd18b633ae8a2ee5de7
jamajk <kmsk1234@o2.pl>
2021-05-12 18:30:17 +0200

    App showcase pages layout finished almost done

--- script.js
+++ script.js
@@ -15,14 +15,17 @@ window.addEventListener('resize', adjust);
 
 
 function hideNavBar() {
-    document.getElementsByTagName("nav")[0].style.top = "-100px";
-    return;
+  document.getElementsByTagName("nav")[0].style.top = "-100px";
+  document.getElementById("logo").style.position = "fixed";
+  document.getElementById("logo").style.top = "20px";
+  document.getElementById("logo").style.borderBottom = "3px solid snow";
+  // document.getElementById("logo").style.padding = "0";
+  return;
 }
 
 function showNavBar() {
-    document.getElementsByTagName("nav")[0].style.top = "0px";
-    return;
-}
-
-function adjust() {
+  document.getElementsByTagName("nav")[0].style.top = "0px";
+  document.getElementById("logo").style.position = "static";
+  document.getElementById("logo").style.border = "none";
+  return;
 }
